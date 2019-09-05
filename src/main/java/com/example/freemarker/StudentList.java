6119b8adaaffa5cc0c0a79ad1e9a9fdb9d36e683
Jimmy Pan <jimmypan@ntu.edu.tw>
2019-09-04 23:18:40 -0700

    Fix StudentList class

--- src/main/java/com/example/freemarker/StudentList.java
+++ src/main/java/com/example/freemarker/StudentList.java
@@ -12,14 +12,11 @@ public class StudentList {
 
     @RequestMapping("list")
     public String list(Model model) {
-//        List<Student> list = new ArrayList<>();
-//        list.add(new Student(1, "Tom", 80.5));
-//        list.add(new Student(2, "Jerry", 90.4));
-//        list.add(new Student(3, "Paul", 77.5));
-//        model.addAttribute("list", list);
-//        return "list";
-        model.addAttribute("message", "Hello Thymeleaf");
-        // return to templates/demo.html page.
+        List<Student> list = new ArrayList<>();
+        list.add(new Student(1, "Tom", 80.5));
+        list.add(new Student(2, "Jerry", 90.4));
+        list.add(new Student(3, "Paul", 77.5));
+        model.addAttribute("list", list);
         return "list";
     }
 }
